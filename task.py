c2e8c2a4e6f00e99d32199993f380211431e7a4d
reger-men <adil-lashab@hotmail.com>
2018-08-18 19:26:24 +0200

    Update task.py

--- task.py
+++ task.py
@@ -22,11 +22,6 @@ PORT2='2224'
 # Define cluster
 cluster_spec = tf.train.ClusterSpec({'worker' : [(IP_ADDRESS1 + ":" + PORT1), (IP_ADDRESS2 + ":" + PORT2)]})
 
-# Task index (integer) should correspond to the IP address of the machine that you are running this notebook on...
-
-# For example, if you are running this notebook on (IP_ADDRESS2 + ":" + PORT2), task_idx=1 because it is 
-# responsible for the second task of the job:worker based on how you defined cluster_spec above
-
 # Define server for specific machine
 task_index = FLAGS.task_index
 server = tf.train.Server(cluster_spec, job_name='worker', task_index=task_index)
